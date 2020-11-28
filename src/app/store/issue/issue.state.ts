3443dea3ee9a9fc2722152df9d2bed229f1d5527
Jimmy <jimmykasprzak@gmail.com>
2020-11-29 00:15:52 +0100

    🎨  normalize issue state

--- src/app/store/issue/issue.state.ts
+++ src/app/store/issue/issue.state.ts
@@ -1,3 +1,27 @@
-export interface IssueState {}
+export interface Issue {
+  id: string;
+  title: string;
+  description: string;
+  priority: 'low' | 'medium' | 'high';
+  resolved: boolean;
+}
 
-export const initialState: IssueState = {};
+export interface Issues {
+  [id: string]: Issue;
+}
+
+export interface Filter {
+  text: string;
+}
+
+export interface IssueState {
+  entities: Issues;
+  filter: Filter;
+}
+
+export const initialState: IssueState = {
+  entities: {},
+  filter: {
+    text: '',
+  },
+};
