99736c8006b3db7c74e5762034b7eb2574f89d3e
Jimmy <jimmykasprzak@gmail.com>
2020-11-28 21:15:12 +0100

    🔧  add store devtools and configure it for dev mode only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { reducers } from 'src/store';
 import { CounterComponent } from './components/counter/counter.component';
+import { modules } from 'src/modules';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { CounterComponent } from './components/counter/counter.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    modules,
   ],
   providers: [],
   bootstrap: [AppComponent]
