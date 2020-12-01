60ee75cec9df8e45c4dadb3212b8518be1bbf28b
Jimmy <jimmykasprzak@gmail.com>
2020-12-01 22:07:17 +0100

    🎨  use immer to create new state from reducer

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,18 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
+import produce from 'immer';
+
 import { submit } from './issue.actions';
 
 import { initialState } from './issue.state';
 
 export const issueReducer = createReducer(
   initialState,
-  on(submit, (state, { issue }) => ({
-    ...state,
-    entities: {
-      ...state.entities,
-      [issue.id]: {
+  on(submit, (state, { issue }) =>
+    produce(state, draft => {
+      draft.entities[issue.id] = {
         ...issue,
         resolved: false,
       }
-    }
-  }))
+    })
+  )
 );
