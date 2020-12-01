aa3b8700d5c6ba967ea3a41786d0a4eb6e6be2bc
Jimmy <jimmykasprzak@gmail.com>
2020-12-01 22:04:18 +0100

    ✨  handle submit issue action

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,5 +1,18 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { submit } from './issue.actions';
 
 import { initialState } from './issue.state';
 
-export const issueReducer = createReducer(initialState);
+export const issueReducer = createReducer(
+  initialState,
+  on(submit, (state, { issue }) => ({
+    ...state,
+    entities: {
+      ...state.entities,
+      [issue.id]: {
+        ...issue,
+        resolved: false,
+      }
+    }
+  }))
+);
