632dcadef1c7e293fef106599b59f3cec29f744f
Jimmy <jimmykasprzak@gmail.com>
2020-12-01 23:23:36 +0100

    ✨ setup basic error handling

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,18 +1,28 @@
-import { createReducer, on } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 import produce from 'immer';
 
 import { submit } from './issue.actions';
 
-import { initialState } from './issue.state';
+import { initialState, IssueState } from './issue.state';
 
-export const issueReducer = createReducer(
+const reducer = createReducer(
   initialState,
   on(submit, (state, { issue }) =>
     produce(state, draft => {
       draft.entities[issue.id] = {
         ...issue,
         resolved: false,
-      }
+      };
     })
   )
 );
+
+export const issueReducer = (state: IssueState | undefined, action: Action): IssueState => {
+  try {
+    return reducer(state, action);
+  } catch (err) {
+    console.error(err);
+
+    return state || initialState;
+  }
+};
