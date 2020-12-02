a8405ef1b21f6f78ea6c0c439e7c639dd13c1b13
Jimmy <jimmykasprzak@gmail.com>
2020-12-02 23:37:50 +0100

    🎨  create selector to select all issues

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { RootState } from 'src/app/store';
+import { selectAll } from 'src/app/store/issue/issue.selectors';
 import { Issue } from 'src/app/store/issue/issue.state';
 
 @Component({
@@ -15,9 +16,7 @@ export class IssueListComponent implements OnInit {
   constructor(private store: Store<RootState>) {}
 
   ngOnInit(): void {
-    this.issues$ = this.store.select(state =>
-      Object.values(state.issue.entities)
-    );
+    this.issues$ = this.store.select(selectAll);
   }
 
 }
