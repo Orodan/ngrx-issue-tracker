c693960cd798e01d3e775c228583ffd57dcd3177
Jimmy <jimmykasprzak@gmail.com>
2020-12-03 23:33:47 +0100

    ✨  create selectors for filtered issues

--- src/app/store/issue/issue.selectors.ts
+++ src/app/store/issue/issue.selectors.ts
@@ -1,4 +1,37 @@
-import { RootState } from '..';
+import { createSelector } from '@ngrx/store';
 
-export const selectAll = (state: RootState) =>
-  Object.values(state.issue.entities);
+import { RootState } from '..';
+import { Filter, Issue } from '../issue/issue.state';
+
+export const selectFeature = (state: RootState) => state.issue;
+
+export const selectEntities = createSelector(
+  selectFeature,
+  ({ entities }) => entities
+);
+
+export const selectAll = createSelector(
+  selectEntities,
+  entities => Object.values(entities)
+);
+
+export const selectFilter = createSelector(
+  selectFeature,
+  ({ filter }) => filter
+);
+
+export const selectFiltered = createSelector(
+  selectAll,
+  selectFilter,
+  (issues: Issue[], { text }: Filter) => {
+    if (!text) { return issues; }
+
+    const lowercasedFilterText = text.toLocaleLowerCase();
+
+    return issues.filter(({ title, description }) =>
+      title.toLowerCase().includes(lowercasedFilterText) ||
+      description.toLowerCase().includes(lowercasedFilterText)
+    );
+  }
+);
+
