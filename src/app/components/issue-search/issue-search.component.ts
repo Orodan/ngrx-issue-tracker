c063fb4ed91536cd5c42ea78f314240a083d3609
Jimmy <jimmykasprzak@gmail.com>
2020-12-04 00:04:10 +0100

    ✨  set filter in store only after 300 ms without new value and when new value is different from previous one

--- src/app/components/issue-search/issue-search.component.ts
+++ src/app/components/issue-search/issue-search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
 import { Store } from '@ngrx/store';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 import { setFilter } from 'src/app/store/issue/issue.actions';
 
 @Component({
@@ -17,6 +20,15 @@ export class IssueSearchComponent implements OnInit {
     this.form = this.fb.group({
       issueFilter: this.fb.control('')
     });
+
+    this.form.get('issueFilter')?.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe(text => {
+        this.store.dispatch(setFilter({ text }));
+      });
   }
 
   submit(): void {
